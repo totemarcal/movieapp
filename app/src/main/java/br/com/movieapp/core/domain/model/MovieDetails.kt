0072b9078a39aa0114d19c0789917986ab6e4ef5
aristotelesenergisa <aristoteles.marcal@energisaterceiros.com.br>
2024-02-08 11:31:10 -0300

    criação do Bottom Navigation Bar.

---
+++ app/src/main/java/br/com/movieapp/core/domain/model/MovieDetails.kt
@@ -0,0 +1,13 @@
+package br.com.movieapp.core.domain.model
+
+data class MovieDetails(
+    val id: Int,
+    val title: String,
+    val genres: List<String>,
+    val overview: String?,
+    val voteAverage: Double = 0.0,
+    val backdropUrl: String = "",
+    val releaseData: String?,
+    val duration: Int = 0,
+    val voteCount: Int = 0
+)
